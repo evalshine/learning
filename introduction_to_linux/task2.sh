4d6c652d8815e544ba50d7a9352307245bc18285
Daniil Kupryianchyk <dankupr21@gmail.com>
2022-12-20 17:34:01 +0300

    task2 done

---
+++ introduction_to_linux/task2.sh
@@ -0,0 +1,9 @@
+if [[ $1 -eq 1 ]]; then
+    echo "$1 student"
+elif [[ $1 -gt 1 && $1 -le 4 ]]; then
+    echo "$1 students"
+elif [[ $1 -ge 5 ]]; then
+    echo "A lot of students"
+else
+    echo "No students"
+fi
