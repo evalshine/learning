706836b92762878793ef11a35f5956e23ea5722c
Daniil Kupryianchyk <dankupr21@gmail.com>
2022-12-21 12:27:29 +0300

    task4 done

--- introduction_to_linux/task3.sh
+++ introduction_to_linux/task3.sh
@@ -1,29 +1,21 @@
-age=-1
-
-while [[ !($age -eq 0) ]]
-do
-    echo "enter your name:"
-    read name
-    if [[ $name = "" ]]
-    then
+#!/bin/bash
+while [[ true ]]; do
+    echo "enter your name:"; read name
+    if [[ -z $name ]]; then 
+        echo "bye" 
+        break 
+    fi
+    echo "enter your age:"; read age
+    if [[ -z $age || $age == 0 ]]; then
         echo "bye"
         break
     fi
-    echo "enter your age:"
-    read age
-    group=""
-    if [[ $age -eq 0 ]]
-    then
-        echo "bye"
-        break
-    fi
-    if [[ $age -le 16 ]]
-    then
-        group="child"
-    elif [[ $age -ge 17 && $age -le 25 ]]
+    group=child
+    if [[ $age -gt 16 && $age -lt 26 ]]
     then
         group="youth"
-    else
+    elif [[ $age -gt 25 ]]
+    then 
         group="adult"
     fi
     echo "$name, your group is $group"
