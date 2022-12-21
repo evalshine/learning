cd14f7c90540895c7fef1b0ccd7359a5c42143cc
Daniil Kupryianchyk <dankupr21@gmail.com>
2022-12-21 12:46:16 +0300

    task4 done

---
+++ introduction_to_linux/task4.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+fun_gcd () 
+{
+    let gcd=0
+    if [[ $1 -eq $2 ]]
+    then
+        gcd=$1
+    elif [[ $1 -gt $2 ]]
+    then
+        let res=$1-$2
+        fun_gcd $res $2
+    elif [[  $1 -lt $2 ]]
+    then
+        let res=$2-$1
+        fun_gcd $1 $res
+    fi
+}
+
+while [[ true ]]
+do
+    read a b
+    if [[ -z $a || -z $b ]]
+    then
+        echo "bye"
+        exit 1
+    fi
+    fun_gcd $a $b
+    echo "GCD is $gcd"
+done
---
+++ introduction_to_linux/task5.sh
@@ -0,0 +1,6 @@
+
+var1=1
+var2=2
+
+let res=$var1+$var2
+echo $res
