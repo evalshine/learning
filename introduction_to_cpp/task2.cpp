48818196bee3f082017486e4368f1b1a2b7f02a2
eval <evalshine@evals-MacBook-Pro.local>
2023-01-04 23:49:20 +0300

    task2 done

--- introduction_to_cpp/task2.cpp
+++ introduction_to_cpp/task2.cpp
@@ -1,7 +1,13 @@
+/*
+N белочек нашли K орешков и решили разделить их поровну. Определите, сколько орешков достанется каждой белочке
+*/
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    cout << "i";
+    int n, k;
+    cin >> n >> k;
+    cout << k / n << endl;
     return 0;
 }
