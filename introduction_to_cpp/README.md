59fc5c2fd98f7199aebbc1bdd4fb748248a4c183
eval <evalshine@evals-MacBook-Pro.local>
2023-01-04 21:36:40 +0300

    init

---
+++ introduction_to_cpp/README.md
@@ -0,0 +1,1 @@
+code example of https://stepik.org/lesson/12908
---
+++ introduction_to_cpp/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int sun(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a << "\n" << b;
+    return 0;
+}
