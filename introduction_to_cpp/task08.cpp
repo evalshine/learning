5bd80540fca2d31e5a0f60b2e3d7dd4281c4efb3
eval <evalshine@evals-MacBook-Pro.local>
2023-01-05 01:13:56 +0300

    task08 done

---
+++ introduction_to_cpp/task08.cpp
@@ -0,0 +1,13 @@
+/*
+На вход дается натуральное число N. Выведите следующее за ним четное число
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n + 2 - n % 2<< endl;
+    return 0;
+}
