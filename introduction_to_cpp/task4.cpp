f7fe9c9bd74d455c8081dcd9111d9134ce45b508
eval <evalshine@evals-MacBook-Pro.local>
2023-01-04 23:52:51 +0300

    task4 done

---
+++ introduction_to_cpp/task4.cpp
@@ -0,0 +1,13 @@
+/*
+Дано натуральное число, выведите его последнюю цифру.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n % 10 << endl;
+    return 0;
+}
